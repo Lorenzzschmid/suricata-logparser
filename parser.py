7fd6c9f69ff525de015317b565dc41ba5b337781
lorenzzschmid <lorenz-schmid@gmx.net>
2025-08-04 20:28:05 +0200

    add alert check function to parser file

---
+++ parser.py
@@ -0,0 +1,14 @@
+import json
+from collections import Counter
+import argparse
+
+def load_alerts(filepath):
+    alerts = []
+    with open(filepath, 'r') as f:
+        for line in f: 
+            try: 
+                event = json.loads(line)
+                if event.get('event_type') == 'alert':
+                    alerts.appen(event)
+                        continue
+    return alerts
